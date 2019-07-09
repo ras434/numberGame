3a404a7d109825591684281443881f37571bd77d
ras434 <ras434@users.noreply.github.com>
2019-07-09 11:12:24 -0400

    Update numberGame.py
    v1.1

--- numberGame.py
+++ numberGame.py
@@ -1,153 +1,160 @@
 #!/usr/bin/env python
-import random, time, sys, platform, curses
+import random
+import time
+import sys
+import platform
+import curses
 from colorama import Fore
 import os
 
-userGuesses = []
+user_guesses = []
 
-WATCHED_FILES = [__file__]		# To add files to watch, create comma seperated list [file1, file2, file3]
+WATCHED_FILES = [__file__]		# To add files to watch, create comma separated list [file1, file2, file3]
 WATCHED_FILES_MTIMES = [(f, os.path.getmtime(f)) for f in WATCHED_FILES]
+
 
 def main():
 	# Initialize the program
 
-	# Check dependancies
+	# Check dependencies
 
-		# Determine if running under Windows OS
+	# Determine if running under Windows OS
 
-		# Determine if colorama is available
+	# Determine if colorama is available
 
-		# Install any Windows specific requirements, if needed
+	# Install any Windows specific requirements, if needed
 
-
-	global userGuesses
-	checkDependancies()
-	# clearScreen()						# might not need this function due to curses features
-	myscreen = curses.initscr()
-	myscreen.border(0)
-	myscreen.addstr(12, 25, "numberGame.py")
-	myscreen.refresh()
-	# myscreen.getch()
+	global user_guesses, last_guess
+	check_dependencies()
+	# clearScreen()  # might not need this function due to curses features
+	my_screen = curses.initscr()
+	my_screen.border(0)
+	my_screen.addstr(12, 25, "numberGame.py")
+	my_screen.refresh()
+	# my_screen.getch()
 	time.sleep(1)
 	curses.endwin()
 	print("Guess a number between 1 and 100.")
 	time.sleep(1)
 	print("Type '" + Fore.YELLOW + "999" + Fore.RESET + "' to exit game.")
 	time.sleep(1)
-	# randomNumber = 35
+	# random_number = 35
 	tries = 0
-	randomNumber = random.randint(1,100)
-	found = False		# flag variable to see if they guessed
-						# it
+	random_number = random.randint(1, 100)
+	found = False  # flag variable to see if they guessed it
 	hint = ""
-	userGuess = ""
-	lastGuess = ""
-	highGuess = 100
-	lowGuess = 1
+	user_guess = 0
+	last_guess = 0
+	high_guess = 100
+	low_guess = 1
 
 # Run through the guessing process
 	while not found:
-		clearScreen()
-		checkFiles()
-		print("Tries: [" + str(tries) + "]\nGuess a number between " + str(lowGuess) + " and " + str(highGuess))
-		if len(userGuesses)>0:
-			print("Last guess: " + str(userGuess) + ", " + hint)
-			hint = ""
-			allGuesses()	# Display all Guesses
+		clear_screen()
+		check_files()
+		print("Tries: [" + str(tries) + "]\nGuess a number between " + str(low_guess) + " and " + str(high_guess))
+		if len(user_guesses) > 0:
+			print("Last guess: " + str(user_guess) + ", " + hint)
+			all_guesses()  # Display all Guesses
 		else:
 			print("\n")
-		if len(userGuesses) > 15:
+		if len(user_guesses) > 15:
 			import subprocess as sp
-			tmp = sp.call('say you should probably try something easier',shell=True)
+			_ = sp.call('say you should probably try something easier', shell=True)
 			print("You should probably try something easier...")
 			exit()
-		try:
-			userGuess = input("Your guess: ")
-			hint = ""
-		except:
+		user_guess = input("Your guess: ")
+		hint = ""
+		if user_guess.isalpha():
 			print("You must use a number!")
 			print("This time, use a number (or type '" + Fore.YELLOW + "999" + Fore.RESET + "' to exit)")
 			time.sleep(3)
 			continue
-		if userGuess == 999:
+		if user_guess == 999:
 			print("Goodbye")
 			exit()
-		if userGuess == 888:
+		if user_guess == 888:
 			print("Restarting...")
 			time.sleep(2)
-			restartMe()
-
-		if userGuess == randomNumber:
+			restart_me()
+		if user_guess == random_number:
 			print("You got it!")
 			time.sleep(1)
 			found = True
-		if userGuess > 100 or userGuess < 1:
-			hint = hint + "You need to guess a number between 1 and 100!"
-		elif userGuess > randomNumber:
-			if userGuess > highGuess:
+		if user_guess > high_guess or user_guess < low_guess:
+			hint = hint + "You need to guess a number between {} and {}!".format(low_guess, high_guess)
+		elif user_guess > random_number:
+			if user_guess > high_guess:
 				hint = hint + "You went too high. "
 			hint = hint + "Guess lower!"
-			highGuess = userGuess
+			high_guess = user_guess
 		else:
-			if userGuess < lowGuess:
+			if user_guess < low_guess:
 				hint = hint + "You went too low. "
 			hint = hint + "Guess higher!"
-			lowGuess = userGuess
-		for x in userGuesses:
-			if userGuess == x:
+			low_guess = user_guess
+		for x in user_guesses:
+			if user_guess == x:
 				hint = hint + " You already picked " + str(x) + "!"
 				pass
-		if highGuess-lowGuess == 2:
-			hint = hint + " I bet you're going to guess " + str(highGuess-1) + " next."
+		if high_guess-low_guess == 2:
+			hint = hint + " I bet you're going to guess " + str(high_guess-1) + " next."
 		else:
-			hint = hint + " Try " + str(int((round((highGuess-lowGuess)/2))+lowGuess)) + "."
-		lastGuess = userGuess
+			hint = hint + " Try " + str(int((round((high_guess-low_guess)/2))+low_guess)) + "."
+		last_guess = user_guess
 		tries += 1
-		userGuesses.append(userGuess)
+		user_guesses.append(user_guess)
 
 	# Print congratulations and goodbye
-	clearScreen()
+	clear_screen()
 	print("Thanks for playing our game!")
 	print("You guessed " + str(tries) + " times.")
-	allGuesses()
+	all_guesses()
 
-def checkFiles():
+
+def check_files():
 	# Check to see if game file has been modified while it is running.
 	# If the file has been modified then restart.
-	for f, mtime in WATCHED_FILES_MTIMES:
-		if os.path.getmtime(f) != mtime:
-			clearScreen()
+	for f, minute_time in WATCHED_FILES_MTIMES:
+		if os.path.getmtime(f) != minute_time:
+			clear_screen()
 			print("File modified.  Restarting game.")
 			time.sleep(2)
-			restartMe()
+			restart_me()
 
-def numberLine():
+
+def number_line():
 	pass
 
-def restartMe():
-	os.execv(__file__, sys.argv)		# TODO: Set this in TRY block in case there is a permission failure here
 
-def allGuesses():
-	global userGuesses
-	print("All guesses: "+ str(userGuesses))
+def restart_me():
+	os.execv(__file__, sys.argv)  # TODO: Set this in TRY block in case there is a permission failure here
 
-def clearScreen():
+
+def all_guesses():
+	global user_guesses
+	print("All guesses: " + str(user_guesses))
+
+
+def clear_screen():
 	import subprocess as sp
 	if platform.system() == "Darwin":
-		tmp = sp.call('clear',shell=True)	# Use 'clear' method for OS X
+		_ = sp.call('clear', shell=True)  # Use 'clear' method for OS X
 	else:
-		tmp = sp.call('cls',shell=True)		# Use 'cls' method for Windows (assuming non OS X is Windows)
+		_ = sp.call('cls', shell=True)  # Use 'cls' method for Windows (assuming non OS X is Windows)
 
-def checkDependancies():
-	for mod in sys.modules:				# TODO: Add list that contains depends. then loop through list
-	# Make sys.modules a list, then compare to dependancies list
+
+def check_dependencies():
+	for mod in sys.modules:  # TODO: Add list that contains depends. then loop through list
+		# Make sys.modules a list, then compare to dependencies list
 		if mod == "colorama":
-			print("dependancies: Found colorama")
+			print("dependencies: Found colorama")
 		if mod == "platform":
-			print("dependancies: Found platform")
+			print("dependencies: Found platform")
 		if mod == "time":
-			print("dependancies: Found time")
+			print("dependencies: Found time")
 	time.sleep(2)
+
 
 if __name__ == "__main__":
 	main()
